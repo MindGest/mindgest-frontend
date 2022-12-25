14fd295fb84136ef8b5cee6ca037dc72d0b49dba
MikeLrUC <miguelrabuge12@gmail.com>
2022-12-25 02:48:18 +0000

    Frontend: Accountant related tasks

---
+++ src/lib/components/MTable.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  export let data = [];
+
+  const sort = field => data.sort((a, b) => (a[field] > b[field]) - (b[field] > a[field]));
+
+  function forward(data) {
+    dispatch('info', {
+      myData: data
+    });
+  }
+</script>
+
+<table class="w-full border-spacing-y-1 border-separate">
+  <thead>
+    <tr>
+      {#each Object.keys(data[0]) as field}
+        <th on:click={() => sort(field)}>{field}</th>
+      {/each}
+    </tr>
+  </thead>
+  <tbody class="text-center">
+    {#each data as row}
+      <tr style='cursor: pointer; cursor: hand;' class="hover:bg-orange-200" on:click={forward(row)}>
+        {#each Object.values(row) as value}
+          <td>{value}</td>
+        {/each}
+      </tr>
+    {/each}
+  </tbody>
+</table>
