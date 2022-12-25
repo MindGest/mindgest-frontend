accfc034c1d995fa373f65872cbaa1548a5df1c9
cabralpinto <jmcabralpinto@gmail.com>
2022-12-25 17:59:05 +0000

    added textbox component

---
+++ src/lib/components/TextBox.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let name = '';
+  export let value = '';
+  const id = name.toLowerCase();
+</script>
+
+<label for={id} class="text-gray-700">{name}</label>
+<input {id} type="text" class="border border-gray-300 rounded w-64 p-2" bind:value />
