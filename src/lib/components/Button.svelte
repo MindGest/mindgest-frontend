d0f1d086ee1d2a6c380991c49c849fb636d282fb
MikeLrUC <miguelrabuge12@gmail.com>
2023-01-11 14:41:20 +0000

    merge

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,13 +1,8 @@
 <script>
-  let class_;
-  export { class_ as class };
-  export let type = 'button';
+    let class_;
+    export { class_ as class };
 </script>
-
-<button
-  {type}
-  class={`text-orange-500 hover:bg-orange-100 w-full  py-2 outline outline-1 rounded ${class_}`}
-  on:click
->
-  <slot /></button
->
+  
+<button class={'text-orange-500 red w-full py-2 outline outline-1 rounded ' + class_} on:click>
+<slot />
+</button>
