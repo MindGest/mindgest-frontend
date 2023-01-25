3f997309aa7f63c5dcf8863ce8b5ffb7e3192054
cabralpinto <jmcabralpinto@gmail.com>
2023-01-25 20:29:42 +0000

    changes checkbox color

--- src/lib/components/Checkbox.svelte
+++ src/lib/components/Checkbox.svelte
@@ -9,7 +9,7 @@
 
 <div class={class_}>
   <label class="cursor-pointer">
-    <input type="checkbox" class="accent-orange-500 mr-1 w-[1.125rem] scale-150 cursor-pointer" bind:checked />
+    <input type="checkbox" class="accent-[#ed6f15] mr-1 w-[1.125rem] scale-150 cursor-pointer" bind:checked />
     {translate(label)}
   </label>
 </div>
