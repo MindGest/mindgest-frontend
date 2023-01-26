d503734a90a58a17ff0f31af8bf4f39243d015d3
Joao Cruz <joao.scruz2000@gmail.com>
2023-01-26 22:37:39 +0000

    integrar user/{intern, therapist}/records/[id]/payments

---
+++ src/routes/user/intern/records/[id]/payments/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import TableMenu from "$lib/components/TableMenu.svelte";
+    import * as api from '$lib/utils/api';
+    import { onMount } from 'svelte';
+
+    let data = []
+
+    onMount(async () => {
+      let process = window.location.href.split("/")[6];
+      const response = await api.get(`process/appointments/${process}`);
+      if (response.ok) {
+        let jsonInfo = await response.json();
+        data = jsonInfo['message'];
+      }
+    });
+</script>
+
+{#if data!=[]}
+<TableMenu
+  {data}
+  object="payment"
+  id="recordPayment"
+  add={false}
+  search={['referencia', 'data']}
+/>
+{/if}
