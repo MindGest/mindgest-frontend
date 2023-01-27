464bf1ea7aa1ac55a2a59207fb74f577013c95e8
MikeLrUC <miguelrabuge12@gmail.com>
2023-01-27 00:47:33 +0000

    Done: Auth frontend

--- src/routes/auth/recover/+page.svelte
+++ src/routes/auth/recover/+page.svelte
@@ -5,19 +5,17 @@
     let password = "";
     let confirm = "";
 
-    async function resetPassword(email, password, confirm) {
-        alert(`${email}, ${password}, ${confirm}`)
+    async function recoverPassword(email) {
+        alert(`${email}`)
         //TODO: Integrate
     }
 </script>
 
 
 <div class="block justify-items-center text-center m-10">
-    <h1 class="text-4xl font-bold m-10">Replace Password</h1>
-    <form on:submit|preventDefault={async () => await resetPassword(email, password, confirm) }>
+    <h1 class="text-4xl font-bold m-10">Recuperar Password</h1>
+    <form on:submit|preventDefault={async () => await recoverPassword(email) }>
         <TextBox class="w-1/3 my-5 m-auto" label="email" bind:value={email} />
-        <TextBox class="w-1/3 my-5 m-auto" label="password" bind:value={password} />
-        <TextBox class="w-1/3 my-5 m-auto" label="Confirmar password" bind:value={confirm} />
         <Button class="w-1/3 my-20 m-auto" text="submit" type="submit" />
     </form>
 </div>
