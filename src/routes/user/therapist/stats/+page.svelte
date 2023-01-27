c69c2ee54d1fcee128ea73a5b4d7bd295ddc625a
MikeLrUC <miguelrabuge12@gmail.com>
2023-01-27 15:08:02 +0000

    stats pdf setup

--- src/routes/user/therapist/stats/+page.svelte
+++ src/routes/user/therapist/stats/+page.svelte
@@ -3,6 +3,8 @@
   import Selector from "$lib/components/Selector.svelte";
   import TextBox from "$lib/components/TextBox.svelte";
   import { onMount } from "svelte";
+  import jsPDF from "jspdf";
+  import autoTable  from "jspdf-autotable";
 
   let start = "";
   let end = "";
@@ -16,7 +18,7 @@
         //TODO: getAvailableRecords(this.user) & getAvailableSpecialities(*all*);
     });
 
-  async function generate() {
+  function generate() {
     let body = {
         "startDate": new Date(start).toISOString(),
         "endDate": new Date(end).toISOString(),
@@ -27,13 +29,135 @@
 
     //TODO: getStatistic()
     console.log(body)
+
+
+
+
+    // let returnData = {
+    //   "totalAppointments": 2,
+    //   "specialitiesAppointments": [
+    //     {"speciality": "cenas1", "total": 23},
+    //     {"speciality": "cenas2", "total": 22},
+    //     {"speciality": "cenas3", "total": 21},
+    //     {"speciality": "cenas4", "total": 13},
+    //     {"speciality": "cenas5", "total": 9},
+    //     {"speciality": "cenas6", "total": 4},
+    //     {"speciality": "a", "total": 0},
+    //     {"speciality": "a", "total": 0},
+    //     {"speciality": "a", "total": 0},
+    //     {"speciality": "a", "total": 0},
+    //     {"speciality": "a", "total": 0},
+    //     {"speciality": "a", "total": 0},
+    //     {"speciality": "a", "total": 0},
+    //     {"speciality": "a", "total": 0},
+    //     {"speciality": "a", "total": 0},
+    //     {"speciality": "a", "total": 0},
+    //   ],
+    //   "processAppointments": [
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //     {"processId": "1","total": 2},
+    //   ],
+    //   "therapistsApointments": [
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //     {"therapist": "teste2", "total": 2},
+    //   ]
+    // }
+    // const pdf = new jsPDF('p','pt', 'a4', true);
+    // pdf.text(20,20, "Estatísticas")
+    // autoTable(pdf, {
+    //   columns: [
+    //     {header: "Especialidade", dataKey: "speciality"},
+    //     {header: "#", dataKey: "total"}
+    //   ],
+    //   body: returnData.specialitiesAppointments
+    // })
+
+    // autoTable(pdf, {
+    //   columns: [
+    //     {header: "Processo", dataKey: "processId"},
+    //     {header: "#", dataKey: "total"}
+    //   ],
+    //   body: returnData.processAppointments
+    // })
+
+    // autoTable(pdf, {
+    //   columns: [
+    //     {header: "Terapeuta", dataKey: "therapist"},
+    //     {header: "#", dataKey: "total"}
+    //   ],
+    //   body: returnData.therapistsApointments
+    // })
+    // pdf.save("test.pdf")
   }
 
 </script>
 
 <h1 class="text-4xl text-center mt-5 mb-10">Gerar estatísticas</h1>
 
-<form on:submit|preventDefault={async () => { await generate() }}>
+<form on:submit|preventDefault={() => { generate() }}>
     <TextBox class="my-5 w-1/2 m-auto" label="Data de Início" type="date" bind:value={start}/>
     <TextBox class="my-5 w-1/2 m-auto" label="Data de Fim" type="date" bind:value={end}/>
     
