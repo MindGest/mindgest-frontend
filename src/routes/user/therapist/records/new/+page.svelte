c943b7ac9df704283b082a0ae6467408fd4703b7
João Alexandre Santos Cruz <joao-s-cruz@telecom.pt>
2023-01-28 12:32:44 +0000

    user/{intern, therapist}/records/new

--- src/routes/user/therapist/records/new/+page.svelte
+++ src/routes/user/therapist/records/new/+page.svelte
@@ -3,12 +3,15 @@
     import Button from '$lib/components/Button.svelte';
     import Selector from '$lib/components/Selector.svelte';
     import { onMount } from 'svelte';
-    //import * as api from '$lib/utils/api';
+    import * as api from '$lib/utils/api';
 
     /* PARA DEBUGGG*/
-    let esp = ['Esp1', 'Esp2', 'Esp3'];
-    let terp = ['Maria', 'João', 'Ana'];
-    let ut = ['Carolina', 'Daniel', 'Fábio'];
+    let esp = [];
+    let terp = [];
+    let ut = [];
+
+    let utId = [];
+    let terpId = [];
 
     let new_proc = {
         esp:'',
@@ -18,15 +21,67 @@
     }
 
     onMount(async () => {
-        /* help */
+        const response = await api.get("patient/list");
+        if(response.ok){
+            let json = await response.json();
+
+            let info = json['message'];
+            let helper = [];
+            for(let i=0; i<info.length; i++){
+                helper.push(info[i]['name'])
+                utId[info[i]['name']]=info[i]['id']
+            }
+            ut = helper;
+        }
+
+        const response1 = await api.get("therapist/list");
+        if(response1.ok){
+            let json = await response1.json();
+
+            let info = json['message'];
+            let helper = [];
+            for(let i=0; i<info.length; i++){
+                helper.push(info[i]['name'])
+                terpId[info[i]['name']]=info[i]['id']
+            }
+            terp = helper;
+        }
+
+        const response2 = await api.get("speciality/list");
+        if(response2.ok){
+            let json = await response2.json();
+
+            let info = json['data'];
+            let helper = [];
+            for(let i=0; i<info.length; i++){
+                helper.push(info[i]['speciality'])
+            }
+            esp = helper;
+        }
+
     });
 
     async function addAppointment() {
-        /* help */
+        let body = {
+            patientIds:[utId[new_proc.ut]],
+            therapistId:terpId[new_proc.terp],
+            speciality:new_proc.esp,
+            remarks:new_proc.mot,
+            colaborators:[]
+        }
+
+        const response = await api.post("process/create", body);
+        if(response.ok){
+            //TODO
+        }
+        else{
+            //TODO
+        }
     }
 
 </script>
 
+{#if esp!=[]}
 <div class="flex flex-col justify-center h-auto w-5/6 m-10 p-5 items-stretch">
     <p class="font-normal text-xl">Novo Processo</p>
     <Selector id="esp" label="Especialidade:" values={esp} bind:value={new_proc.esp}/>
@@ -37,3 +92,4 @@
 <div class="my-5 grid grid-cols-2" style="position:relative; left:60%; width:50%">
     <Button text="Enviar pedido de criação de processo" class="mt-10 " on:click={addAppointment}/>
 </div>
+{/if}
