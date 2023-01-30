91747b9063ed9ba1f413219d251c0253855fbde6
cabralpinto <jmcabralpinto@gmail.com>
2023-01-30 15:19:40 +0000

    refactored & fixed navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,17 +1,29 @@
 <script>
-  export let buttons = [];
-  export let href = '';
-  const style = href_ => (href_ === href ? 'text-orange-500 underline underline-offset-4' : '');
+  import { page } from '$app/stores';
+  import navbar from '$lib/data/navbar.json';
+  import Link from './Link.svelte';
+
+  const role = $page.url.pathname.split('/')[2];
+  const buttons = Object.keys(import.meta.glob('/src/routes/user/*/*/+page.svelte'))
+    .map(path => path.split('/').slice(4, 6))
+    .filter(([role_, page]) => role_ === role && page !== 'profile')
+    .sort(([, page1], [, page2]) => navbar.indexOf(page1) - navbar.indexOf(page2))
+    .map(([role, page]) => ({ text: page, href: `/user/${role}/${page}` }));
 </script>
 
-<nav class="w-full fixed top-0 p-4 flex items-center shadow-md bg-white">
-  <img src="/img/logo.png" alt="logo" class="h-12" />
-  {#each buttons as { text, href }}
-    <a {href} class={`ml-5 ${style(href)}`}>{text}</a>
-  {/each}
-  <div class="ml-auto">
-    <a {href} class={`ml-5 ${style(href)}`}>{text}</a>
-      <a {href} class={`ml-5 ${style(href)}`}>{text}</a>
+<nav class="w-full fixed top-0 p-4 flex bg-zinc-50 shadow-sm border border-b-zinc-400">
+  <div class="flex items-center space-x-5">
+    <img src="/img/logo.png" alt="logo" class="h-12" />
+    {#each buttons as { text, href }}
+      <Link {href} active={$page.url.pathname.startsWith(href)} {text} />
     {/each}
+  </div>
+  <div class="ml-auto flex items-center space-x-5">
+    <Link
+      href="/user/{role}/profile"
+      active={$page.url.pathname.startsWith(`/user/${role}/profile`)}
+      text="profile"
+    />
+    <Link href="/auth/logout" text="logout" />
   </div>
 </nav>
