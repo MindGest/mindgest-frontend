a02ebc0c33b205f260ec763f0940b1717dee69d5
joanaa-b <81414109+joanaa-b@users.noreply.github.com>
2023-02-01 19:05:19 +0000

    Frontend only
    user/admin/appointments/[id]
    user/admin/users/[id]/edit
    user/intern/appointments/new
    user/intern/appointments/[id]

---
+++ src/routes/user/admin/users/[id]/edit/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { page } from '$app/stores';
+  import EditProfile from "$lib/menus/EditProfile.svelte";
+  import Button from "$lib/menus/Button.svelte";
+  const id = $page.params['id'];
+</script>
+
+<EditProfile self={false} {id}/>
---
+++ src/routes/user/intern/appointments/[id]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Details from "$lib/components/Details.svelte";
+
+  let data={
+    record: '',
+    room: '',
+    start: 0,
+    end: 0,
+    date: ''
+  }
+
+</script>
+<Details
+    data={data}/>
