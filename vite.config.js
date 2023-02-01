15777651657e6810e223a08d6337cceae74b95c6
Pedro Rodrigues <pedromiguelrodrigues2000@gmail.com>
2023-02-01 14:16:52 +0000

    Fixing Build Config

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,30 @@ import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = defineConfig({
-  plugins: [sveltekit()],
-  build: {
-    rollupOptions: {
-      external: [
-        'svelte-material-icons/CalendarAccount.svelte',
-        'svelte-material-icons/Eye.svelte',
-        'svelte-material-icons/Pencil.svelte',
-        'svelte-material-icons/ChartBar.svelte',
-        'svelte-material-icons/AccountCircle.svelte',
-        'svelte-material-icons/Folder.svelte',
-        'http-status-codes',
-        'jspdf',
-        'jspdf-autotable'
-      ]
-    }
-  }
+  plugins: [
+    sveltekit({
+      output: {
+        rollupOptions: {
+          sourcemap: true,
+          format: 'iife',
+          name: 'app',
+          file: 'public/build/bundle.js',
+          inlineDynamicImports: true
+        },
+        external: [
+          'svelte-material-icons/CalendarAccount.svelte',
+          'svelte-material-icons/Eye.svelte',
+          'svelte-material-icons/Pencil.svelte',
+          'svelte-material-icons/ChartBar.svelte',
+          'svelte-material-icons/AccountCircle.svelte',
+          'svelte-material-icons/Folder.svelte',
+          'http-status-codes',
+          'jspdf',
+          'jspdf-autotable'
+        ]
+      }
+    })
+  ]
 });
 
 export default config;
